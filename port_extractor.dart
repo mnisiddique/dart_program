71c289f292f42dadfe03b9dff8d41d8cc5f169dc
Prince <mnisiddique@ael-bd.com>
2024-03-28 15:30:22 +0600

    initial commit

---
+++ port_extractor.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() async {
+  PortExtractor pe = PortExtractor();
+  final port = await pe.extractPort();
+  print("port: $port");
+}
+
+class PortExtractor {
+  static const _kJsonKeyForPort = "port";
+  static const _kJsonKeyForEnvPath = "envPath";
+  static const _mockEnvMetaPath = "mock_env_meta.json";
+
+  Future<String> extractPort() async {
+    final metaMap = await decodeJson(_mockEnvMetaPath);
+    final envMap = await decodeJson(metaMap[_kJsonKeyForEnvPath]);
+    return envMap[_kJsonKeyForPort].toString();
+  }
+
+  Future<Map<String, dynamic>> decodeJson(String jsonFilePath) async {
+    final jsonFile = File(jsonFilePath);
+    final jsonStr = await jsonFile.readAsString();
+    final Map<String, dynamic> map = json.decode(jsonStr);
+    return map;
+  }
+}
